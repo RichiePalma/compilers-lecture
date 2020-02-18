c926421872255a5dab021963f3eff8f0c60f9daa
Ricardo Palma <palmamendoza.5@hotmail.com>
2020-02-18 11:54:58 -0600

    Tokenized source code

--- labs/01/asm-analitics.sh
+++ labs/01/asm-analitics.sh
@@ -1,5 +1,18 @@
-#!/usr/bin/bash
-
-echo 'hi, this is done :)'
-
-echo 'mistake'
+#!/usr/bin/python3
+#!/usr/bin/pip3
+import sys
+ 
+def tokenize(file):
+    f=open(file,"r")
+    if f.mode == "r":
+        text = f.read()
+        tokens = text.split("\t")
+       # print('[%s]' % ', |||'.join(map(str, tokens)))
+        count = 1
+       for token in tokens:
+            if(count % 3 == 0):
+                print (token)
+            count += 1
+            
+#print (sys.argv[1])
+tokenize(sys.argv[1])
